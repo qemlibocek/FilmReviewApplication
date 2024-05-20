fa18dde871fe697dfb06e2829aa9fe5c1b58862f
Murad <muradsixmuradov@gmail.com>
2024-05-20 15:30:42 +0400

    Small changings

--- src/main/java/com/example/filmreviewapplication/model/entity/UserProfile.java
+++ src/main/java/com/example/filmreviewapplication/model/entity/UserProfile.java
@@ -1,9 +1,43 @@
 package com.example.filmreviewapplication.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Table(name = "user_Profiles")
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProfile {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 50)
+    Long id;
+    @Column(nullable = false)
+    String firstName;
+    @Column(nullable = false)
+    String lastName;
+    @Column(nullable = false, unique = true)
+    String username;
+    @Column(nullable = false, unique = true)
+    String password;
+    @Column(nullable = false)
+    Integer age;
+    @Column(unique = true)
+    String phoneNumber;
+    String country;
 
+    @CreationTimestamp
+    LocalDateTime createdAt;
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
 
+    Boolean isActive = true;
 
 }
