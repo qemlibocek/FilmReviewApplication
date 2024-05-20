af888e2434092899717b66fc7e4d8521c4cab3c8
Murad <muradsixmuradov@gmail.com>
2024-05-20 05:36:02 +0400

    Entrance

---
+++ src/main/java/com/example/filmreviewapplication/FilmReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmreviewapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmReviewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/filmreviewapplication/model/UserProfile.java
@@ -0,0 +1,4 @@
+package com.example.filmreviewapplication.model;
+
+public class UserProfile {
+}
